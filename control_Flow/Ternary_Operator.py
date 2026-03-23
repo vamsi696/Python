29b4bd4a7ce5e6fe1088b8a3735c76e77c69d081
Hemanth Kandalla <vamsilionel@gmail.com>
2026-03-23 23:36:30 +0530

    Worked on Control Flow of Operators

---
+++ control_Flow/Ternary_Operator.py
@@ -0,0 +1,7 @@
+age = 12
+
+message = "Eligible" if age >= 18 else "Not Eligible"  # This is ternary operator,
+# where we can write the same condition in if else block,
+# writing in single line is known as ternary operator
+
+print(message)
